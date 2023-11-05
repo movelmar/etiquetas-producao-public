cc4dfb3616448117a534a6d6a0b5c93e24f24fb2
fernando spanhol <ferspanhol@gmail.com>
2023-11-04 21:46:59 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import Head from "next/head";
-import { redirect } from 'next/navigation'
 import styles from "./index.module.css";
 
 export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Movelmar</title>
-      </Head>
       <main className={styles.main}>
         redirecionando...
       </main>
